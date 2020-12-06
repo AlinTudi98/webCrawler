de3aaad41d3023401746fa82675c1157c67d0431
Ciobanu Cosmin <ciobanu.cosmin998@gmail.com>
2020-12-06 19:29:20 +0200

    Finalizare constructor si metoda de verificare a URL-ului
    Issue: #8
    
    Implementeaza clasa Robot cu un constructor ce initializeaza lista
    de link-uri ce nu pot fi accesate de crawler si delay-ul specific paginii
    si ofera metode de acesare a elementelor componente precum si
    o metoda de verificare a unui site daca poate fi accesat sau nu.
    
    Signed-off-by: Ciobanu Cosmin <cosmin.ciobanu@mta.ro>

--- src/Robot.java
+++ src/Robot.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 /**
  * This class deals with the extraction of information from
@@ -18,22 +22,67 @@ public class Robot {
 
     public boolean verifyURL(String Url){
 
+        for(String element : disallowUrls){
+            if(element.equals(Url)){
+                return false;
+            }
+        }
+
         return true;
     }
 
     public Robot() {
+        disallowUrls = new ArrayList<String>();
+        crawlDelay = 0;
     }
 
     public Robot(URL url) throws MalformedURLException{
+        this();
+        baseUrlOfRobot = url;
+
+        try {
+            String buffer;
+            URLConnection connection;
+            String site;
+            BufferedReader inStream;
+            String agent = "";
+
+            site = url.toString()+"/robots.txt";
+            url = new URL(site);
+            connection = url.openConnection();
+            connection.connect();
+
+            inStream= new BufferedReader(new InputStreamReader(url.openStream()));
+            while ((buffer = inStream.readLine()) != null){
+                StringTokenizer token = new StringTokenizer(buffer," ");
+                while (token.hasMoreElements()){
+                    String tokenLine = token.nextToken();
+                    if (tokenLine.equals("User-Agent:")) {
+                        if(agent.equals("*")){
+                            inStream.close();
+                            return;
+                        }
+                        agent = token.nextToken();
+                    }
+                    if (tokenLine.equals("Crawl-delay:")) {
+                        crawlDelay=Integer.parseInt(token.nextToken());
+                    }
+                    if (tokenLine.equals("Disallow:") && agent.equals("*") ) {
+                        disallowUrls.add(token.nextToken());
+                    }
+                }
+            }
+            inStream.close();
+        }catch (Exception exception){
+            System.out.println("Robots inexistent on site: " + url.toString());
+        }
     }
 
     public URL getbaseUrlOfRobot() {
-
         return baseUrlOfRobot;
     }
 
     public int getCrawlDelay() {
-
         return crawlDelay;
     }
 }
