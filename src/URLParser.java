46f51d857a2f7b9032d13f0e106d4286739f0658
Alin Tudose <alintudose126@gmail.com>
2020-12-08 19:43:46 +0200

    Implementare metoda URLParser:validateURL()
    Issue: #27
    Metoda verifica daca un string pasat ca parametru respecta formatul
    valid al unui URL.
    
    Signed-off-by: Alin Tudose <alintudose126@gmail.com>

--- src/URLParser.java
+++ src/URLParser.java
@@ -1,8 +1,20 @@
 import java.io.File;
+import java.util.regex.*;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class URLParser {
 
     boolean validateURL(String url){
+
+        String urlRegex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+        Pattern urlPattern = Pattern.compile(urlRegex);
+        Matcher urlMatcher = urlPattern.matcher(url);
+
+        String testUrl = urlMatcher.group(1);
+
+        if(url.equalsIgnoreCase(testUrl))
+            return true;
         return false;
     }
 
@@ -11,6 +23,6 @@ public class URLParser {
     }
 
     void parse(String urlString){
-        
+
     }
 }
