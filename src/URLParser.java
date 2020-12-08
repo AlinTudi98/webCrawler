535523607d76280bfa0e62c23a5dcb7339b6850e
Alin Tudose <alintudose126@gmail.com>
2020-12-08 19:11:41 +0200

    Implementare Schelet Clasa URLParser
    Issue: #25
    Implementeaza scheletul clasei ce va fi folosita pentru a extrage
    linkurile ce se doresc a fi downloadate din fisierul sau stringul pasat
    ca parametru in linia de comanda.
    
    Signed-off-by: Alin Tudose <alintudose126@gmail.com>

---
+++ src/URLParser.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class URLParser {
+
+    boolean validateURL(String url){
+        return false;
+    }
+
+    void parse(File urlFile){
+
+    }
+
+    void parse(String urlString){
+        
+    }
+}
