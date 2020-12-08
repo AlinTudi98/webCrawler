6d23116fd71a0ce5d5c74b9356d1b2306d0c4bc7
Ciobanu Cosmin <ciobanu.cosmin998@gmail.com>
2020-12-08 18:22:05 +0200

    Implementare schelet StackManager
    Issue: #11
    
    Clasa StackManager cu toate metodele si membrii necesari
    folosirii ulterioare de catre celelalte elemente din program.
    
    Signed-off-by: Ciobanu Cosmin <cosmin.ciobanu@mta.ro>

--- src/StackManager.java
+++ src/StackManager.java
@@ -1,10 +1,11 @@
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class StackManager {
-    Stack<URLString> urlStack;
     private static StackManager stackmanager_instance = null;
-
+    private Stack<URLString> urlStack;
+    private ArrayList<Robot> robotsList;
+    private final Object lock = new Object();
     private StackManager(){ ; }
 
     public static StackManager getInstance(){
@@ -12,5 +13,28 @@ public class StackManager {
             stackmanager_instance = new StackManager();
         }
         return stackmanager_instance;
+    }
+
+    public void PushURL(URLString url){
+        synchronized (lock){
+
+        }
+    }
+
+    public URLString PopURL(){
+        synchronized (lock){
+
+        }
+        return null;
+    }
+
+    public void addRobot(URLString url){
+        synchronized (lock){
+
+        }
+    }
+
+    public int getDelayForRobot(URLString url){
+        return 0;
     }
 }
