9a5fbb0a46f12c2ffd7bae29da6546039a1f4e66
Andreea Avram <andreea.avram98@yahoo.com>
2020-12-09 15:16:16 +0200

    Implementare functie help
    Issue: #46
    
    Signed-off-by: Andreea Avram <andreea.avram98@yahoo.com>

--- src/Config.java
+++ src/Config.java
@@ -20,7 +20,19 @@ public class Config {
         if(ConfigInstance == null) {
             ConfigInstance = new Config(filename);
         }
-
         return ConfigInstance;
     }
+
+    public void help(){
+        System.out.println("Fisierul de configurare are urmatoarea structura:\n");
+        System.out.println(("numThreads = <nr>\n" +
+                            "delay = <nr>\n" +
+                            "rootDir = <path>\n" +
+                            "logLevel = <nr>\n" +
+                            "ignoreRobots = <1/0>\n" +
+                            "logFilename = <path>\n" +
+                            "dSizeLimit = <nr>"));
+    }
+    
+
 }
