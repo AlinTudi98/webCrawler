113fef4f705445e56a36899b74aa8c4aa5491836
Alex Ghita <alexandru.ghita98@gmail.com>
2020-12-09 17:33:53 +0200

    Implementare schelet clasa Sitemap
    Issue: #33
    
    Implementeaza scheletul clasei Sitemap pentru a putea realiza
    sitemap-ul unui site recent descarcat.
    
    Signed-off-by: Alex Ghita <alexandru.ghita98@gmail.com>

---
+++ src/Sitemap.java
@@ -0,0 +1,12 @@
+public class Sitemap {
+
+    private String path;
+
+    public Sitemap(String path){
+        this.path = path;
+    }
+
+    public boolean getSiteMap(){
+        return true;
+    }
+}
