e18a5c45cb1a647fa6c82420d7e655a7d823f009
Alin Tudose <alintudose126@gmail.com>
2020-12-09 19:57:47 +0200

    Logare actiuni URLParser:parse() pentru fisiere
    Issue: #1
    Au fost adaugate instructiunile pentru logarea activitatii clasei
    URLParser in functia parse pentru fisiere.
    
    Signed-off-by: Alin Tudose <alintudose126@gmail.com>

--- src/URLParser.java
+++ src/URLParser.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.regex.*;
@@ -23,33 +24,41 @@ public class URLParser {
     }
 
     public void parse(File urlFile) {
-        StackManager stackManager = StackManager.getInstance();
         try {
-            Scanner reader = new Scanner(urlFile);
+            Logger logger = Logger.getInstance();
+            try {
 
+                StackManager stackManager = StackManager.getInstance();
+                Scanner reader = new Scanner(urlFile);
 
-            while (reader.hasNextLine()) {
-                String line = reader.nextLine();
+                while (reader.hasNextLine()) {
+                    String line = reader.nextLine();
 
-                if (validateURL(line)) {
-                    try {
-                        URL url = new URL(line);
-                        URLString urlString = new URLString(url, 0);
-                        stackManager.PushURL(urlString);
-                        //Logger.log(Pushed line);
-                    } catch (MalformedURLException e) {
-                        //Logger.log(Ignored line);
+                    if (validateURL(line)) {
+                        try {
+                            URL url = new URL(line);
+                            URLString urlString = new URLString(url, 0);
+                            stackManager.PushURL(urlString);
+                            logger.log(LogCode.INFO, "[INFO] URLParser: Added URL: \"" + line + "\" to download stack.");
+
+                        } catch (MalformedURLException e) {
+                            logger.log(LogCode.WARN, "[WARN] URLParser: MalformedURLException thrown for line: \"" + line + "\". Line has been ignored.");
+                        }
+                    } else {
+                        logger.log(LogCode.WARN, "[WARN] URLParser: Ignored line: \"" + line + "\". Not a valid URL.");
                     }
-                } else {
-                    //Logger.log(Ignored line);
+
                 }
+                reader.close();
 
             }
-            reader.close();
+            catch (FileNotFoundException ee) {
+                logger.log(LogCode.FATAL,"[FATAL] URLParser: Could not open input file.");
+            }
         }
-        catch(FileNotFoundException ee)
+        catch(IOException e)
         {
-            //Logger.log(Fatal, file not found)
+            System.out.println("[FATAL]: Could not get instance of logger");
         }
 
     }
