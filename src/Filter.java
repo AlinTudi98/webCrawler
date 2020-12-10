01e52d90e12b3e402809404f72ee17f802c5dc69
Alex Ghita <alexandru.ghita98@gmail.com>
2020-12-10 12:21:49 +0200

    Implementare metoda search() in clasa Filter
    Issue: #69
    Am implementat metoda search(), care pe baza
    unui String ce contine numele unui director dat
    ca parametru, afiseaza toate fisierele cu dimensiunea
    cel mult egala cu cea instantiata in clasa, precum
    si cu tipul existent.
    Am fixat cateva aspecte de afisare: cand se apeleaza logger-ul
    acesta adauga automat "\n", nemaifiind necesara adaugarea in
    prealabil
    
    Signed-off-by: Alex Ghita <alexandru.ghita98@gmail.com>

--- src/Filter.java
+++ src/Filter.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Filter {
 
     private final String type;
@@ -8,8 +12,39 @@ public class Filter {
         this.size = size;
     }
 
-    public boolean search(String directory) {
-        return true;
+    private String getFileExtension(String fileName) {
+        String extension = "";
+
+        int i = fileName.lastIndexOf('.');
+        if (i > 0) {
+            extension = fileName.substring(i+1);
+            return extension;
+        }
+        else {
+            return "txt";
+        }
+
     }
 
+    public boolean search(String directory) throws IOException {
+        Files.walk(Paths.get(directory))
+                .filter(Files::isRegularFile)
+                .forEach(filePath -> {
+                    try {
+                        if(Files.size(filePath) <= size && getFileExtension(filePath.getFileName().toString()).equals(type)) {
+                            System.out.println("Name: "+filePath.getFileName().toString() +
+                                    "; Size: " + Files.size(filePath) + "(bytes); " +
+                                    "Directory: " + filePath.getParent().toString());
+                        }
+                    } catch (IOException e) {
+                        try {
+                            Logger.getInstance().log(LogCode.FATAL, "Filter was unable to access given path!");
+                        } catch (IOException ioException) {
+                            System.out.println("Unexpected error occurred! Logger cannot be created!");
+                        }
+                    }
+                });
+
+        return true;
+    }
 }
