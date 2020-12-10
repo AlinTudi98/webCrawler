7e92e3a97d7b2fe039c8ca569839fbd65991ad50
Alex Ghita <alexandru.ghita98@gmail.com>
2020-12-10 14:39:24 +0200

    Documentare clasa Filter
    Issue: #74
    Documentarea clasei Filter si a metodelor si membrilor
    prezenti in aceasta.
    
    Signed-off-by: Alex Ghita <alexandru.ghita98@gmail.com>

--- src/Filter.java
+++ src/Filter.java
@@ -2,7 +2,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+/**
+ * The purpose of this class is to retrieve certain files in a given
+ * path that match the criteria of size and type.
+ *
+ * @author Ghita Alexandru-Andrei
+ *
+ */
+
 public class Filter {
+
+    /**
+     * Members of class Filter
+     * type: the type of the file that we are querying for. Example:
+     *       txt, pdf, html
+     * size: the maximum size that the file must have in order to match
+     *       the criteria
+     */
 
     private final String type;
     private final long size;
@@ -11,6 +27,15 @@ public class Filter {
         this.type = type;
         this.size = size;
     }
+
+    /**
+     * Method used to retrieve a file extension. For example
+     * if we have "output.pdf" it will retrieve "pdf". If no
+     * extension is present, it will assume it is a <i>.txt</i> file
+     *
+     * @param fileName the name of the file containing the extension
+     * @return the extension as a <i>String</i>
+     */
 
     private String getFileExtension(String fileName) {
         String extension = "";
@@ -25,6 +50,17 @@ public class Filter {
         }
 
     }
+
+    /**
+     *
+     * Method used to print the results consisting of the file name, size and location, that match
+     * the <b>type</b> and <b>size</b> arguments.
+     *
+     * @param directory the directory given as the location to search for files that
+     *                  match the criteria
+     * @return true if the operation has not encountered any errors
+     * @throws IOException if the path cannot be accessed by the FileWriter
+     */
 
     public boolean search(String directory) throws IOException {
         Files.walk(Paths.get(directory))
