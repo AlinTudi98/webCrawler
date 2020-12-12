664a783015a78c2bf7320b45f03b52369b144883
Alin Tudose <alintudose126@gmail.com>
2020-12-12 18:47:51 +0200

    Implementare Main:readArgs()
    Issue: #89
    Metoda readArgs creaza obiectul de tip config, si suprascrie parametri
    dati din linie de comanda in acesta.
    Closes: #89
    
    Signed-off-by: Alin Tudose <alintudose126@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,48 @@ public class Main {
         ;
     }
 
-    private void readArgs(String[] args, Config config) {
-        String[] posParams = {"crawl","sitemap","list","search",
-                "-startLinks=","-inFile=","-maxDepth=","-dTypes=",
-                "-ignoreRobots","-config=","-maxSize=","-rootDir=",
-                "-keyWords="};
+    private void readArgs(String[] args) {
+        String[] posParams = {
+                "-maxDepth=", "-dTypes=",
+                "-ignoreRobots", "-config=", "-maxSize=", "-rootDir=",
+        };
 
-        for(String iter: args)
-        {
+        Config config = loadConfig("config.txt");
 
+        for (String iter : args) {
+            if (iter.equalsIgnoreCase("-config=")) {
+                config = loadConfig(iter.substring(8));
+            }
+        }
+
+        for (String arg : args) {
+            if (arg.equalsIgnoreCase("-maxDepth=")) {
+                config.maxDepth = Integer.parseInt(arg.substring(10));
+            }
+
+            if (arg.equalsIgnoreCase("-dTypes=")) {
+                String types = arg.split("=")[1];
+                config.dTypes = types.split(",");
+            }
+
+            if (arg.equalsIgnoreCase("-ignoreRobots")) {
+                config.ignoreRobots = 1;
+            }
+
+            if (arg.equalsIgnoreCase("-maxSize=")) {
+                config.dSizeLimit = Integer.parseInt(arg.substring(9));
+            }
+            if (arg.equalsIgnoreCase("-rootDir=")) {
+                config.rootDir = arg.substring(9);
+            }
         }
     }
 
-    private Config loadConfig(String configPath){
+    private Config loadConfig(String configPath) {
         return Config.getInstance(configPath);
     }
 
-    private boolean processCommand(Config config, String[] args)
-    {
+    private boolean processCommand(Config config, String[] args) {
         return true;
     }
 
